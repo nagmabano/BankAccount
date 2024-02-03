84673743777ad0525ae9c92840f571fa9be300e1
Nagma Bano <nagma.bano@sap.com>
2024-02-03 14:18:49 +0530

    BankAccount operations

--- app/src/main/java/com/learning/bankaccount/BankAccount.kt
+++ app/src/main/java/com/learning/bankaccount/BankAccount.kt
@@ -5,14 +5,28 @@ class BankAccount(var accountHolder:String, var balance: Double) {
     private val transactionHistory = mutableListOf<String>()
 
     fun deposit(amount:Double) {
-
+        balance += amount
+        transactionHistory.add("$accountHolder deposited $$amount")
     }
 
     fun withdraw(amount: Double) {
+        if (amount <= balance)
+        {
+            // can withdraw the money
+            balance -= amount
+            transactionHistory.add("$accountHolder withdrew $$amount")
+        } else {
+            // can not withdraw the money
+            println("You don't have the funds to withdraw $$amount")
+        }
 
     }
 
     fun displayTransactionHistory() {
+        println("Transaction history for $accountHolder")
+        for (transaction in transactionHistory) {
+            println(transaction)
+        }
 
     }
 }
--- app/src/main/java/com/learning/bankaccount/BankMain.kt
+++ app/src/main/java/com/learning/bankaccount/BankMain.kt
@@ -3,7 +3,12 @@ package com.learning.bankaccount
 fun main() {
 
     val jennyBankAccout = BankAccount("Jenny Doe", 1342.20)
-    println(jennyBankAccout.accountHolder)
-    println(jennyBankAccout)
+    jennyBankAccout.deposit(200.0)
+    jennyBankAccout.withdraw(1200.0)
+    jennyBankAccout.deposit(100.0)
+    jennyBankAccout.withdraw(50.0)
+    jennyBankAccout.displayTransactionHistory()
+
+    println("${jennyBankAccout.accountHolder} has balance of $${jennyBankAccout.balance}")
 
 }
